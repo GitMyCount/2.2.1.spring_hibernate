1eb7410959b9e9c7044a5336c924c5387ab5df29
user <dvd.off@mail.ru>
2021-02-21 20:33:58 +0700

    add commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,60 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import hiber.service.UserServiceImp;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+      Car car1 = new Car("vaz", 2101);
+      Car car2 = new Car("vaz", 2102);
+      Car car3 = new Car("vaz", 2102);
+      Car car4 = new Car("vaz", 2104);
+
+      user1.setUserCar(car1);
+      user2.setUserCar(car2);
+      user3.setUserCar(car3);
+      user4.setUserCar(car4);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getUserCar());
+         System.out.println();
+      }
+
+      String model = "vaz";
+      int series = 2102;
+      List<User> users2 = userService.findUser(model, series);
+            for (User user : users2) {
+         System.out.println("Авто, модель - " + model + "; серии - " + series +
+                 "; принадлежит - " + user.getFirstName());
+      }
+
+      context.close();
+   }
+}
